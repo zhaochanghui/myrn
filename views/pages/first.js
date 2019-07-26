8d38fd1d78c33fd2ecebe5ae2dae1fa74f1d6bbc
zhaochanghui <zhaochanghui@leyue100.com>
2019-07-26 18:50:35 +0800

    antd-rn库加入

--- views/pages/first.js
+++ views/pages/first.js
@@ -1,91 +1,70 @@
-import React, { Component } from 'react';
-import {View,Text,Button,StyleSheet,Image,TextInput,TouchableOpacity,FlatList,Dimensions }  from 'react-native';
-import { ButtonGroup ,Header,ListItem } from 'react-native-elements';
-
-//http://www.developer1.cn:8001/bsj/index.php
-
-class First extends Component{
-    constructor(props) {
-        super(props);
-        this.state = {
-            data:[],
-            selectedIndex:1,
-              loaded:false,
-        }
-        this.updateIndex = this.updateIndex.bind(this);
-        this.fetchData = this.fetchData.bind(this);
-    }
-    updateIndex(selectedIndex) {
-        this.setState({ selectedIndex })
-    }
-
-    fetchData(){
-        fetch('http://www.developer1.cn:8001/bsj/index.php')
-            .then((response) => response.json())
-            .then((responseData) => {
-                // 注意，这里使用了this关键字，为了保证this在调用时仍然指向当前组件，我们需要对其进行“绑定”操作
-                console.log(responseData);
-                this.setState({
-                    data: this.state.data.concat(responseData),
-                    loaded: true,
-                });
-            });
-    }
-
-    componentDidMount(){
-        this.fetchData();
-    }
-
-    keyExtractor = (item, index) => index.toString()
-    renderItem = ({ item }) => (
-      <ListItem
-      style={{marginBottom:5}}
-        title={item.name}
-        subtitle={item.price}
-        leftAvatar={{
-          source: item.avatar_url && { uri: item.avatar_url },
-          title: item.name
-        }}
-      />
-    )
-
-    render() {
-      if(!this.state.loaded){
-          return(
-              <View style={{flex:1,flexDirection:"row",justifyContent:"center",alignItems:'center',backgroundColor:"#F5FCFF"}}>
-                  <Text>正在加载数据...</Text>
-              </View>
-          );
-      }
-
-      const list = [
-        {
-          name: 'Amy Farha',
-          avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
-          price: 'Vice President'
-        },
-        {
-          name: 'Chris Jackson',
-          avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
-          price: 'Vice Chairman'
-        },
-        {
-          name: '朋友111',
-          avatar_url: 'https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=585605422,3072805408&fm=11&gp=0.jpg',
-          price: 'laojiade'
-        }
-      ]
-
-      return(
-        <FlatList
-             keyExtractor={this.keyExtractor}
-             data={this.state.data}
-             renderItem={this.renderItem}
-          />
-        );
-
-    }
+import React from 'react';
+import { ScrollView, Text, View } from 'react-native';
+import { Tabs } from '@ant-design/react-native';
+const renderContent = (tab, index) => {
+  const style = {
+    paddingVertical: 40,
+    justifyContent: 'center',
+    alignItems: 'center',
+    margin: 10,
+    backgroundColor: '#ddd',
+  };
+  const content = [1, 2, 3, 4, 5, 6, 7, 8].map(i => {
+    return (
+      <View key={`${index}_${i}`} style={style}>
+        <Text>
+          {tab.title} - {i}
+        </Text>
+      </View>
+    );
+  });
+  return <ScrollView style={{ backgroundColor: '#fff' }}>{content}</ScrollView>;
+};
+export default class BasicTabsExample extends React.Component {
+  render() {
+    const tabs = [
+      { title: 'First Tab' },
+      { title: 'Second Tab' },
+      { title: 'Third Tab' },
+    ];
+    const tabs2 = [
+      { title: '1st Tab' },
+      { title: '2nd Tab' },
+      { title: '3rd Tab' },
+      { title: '4th Tab' },
+      { title: '5th Tab' },
+      { title: '6th Tab' },
+      { title: '7th Tab' },
+      { title: '8th Tab' },
+      { title: '9th Tab' },
+    ];
+    const style = {
+      alignItems: 'center',
+      justifyContent: 'center',
+      height: 150,
+      backgroundColor: '#fff',
+    };
+    return (
+      <View style={{ flex: 1 }}>
+        <Tabs tabs={tabs}>
+          <View style={style}>
+            <Text>Content of First Tab</Text>
+          </View>
+          <View style={style}>
+            <Text>Content of Second Tab</Text>
+          </View>
+          <View style={style}>
+            <Text>Content of Third Tab</Text>
+          </View>
+        </Tabs>
+        <View style={{ flex: 2 }}>
+          <Tabs tabs={tabs2} initialPage={1} tabBarPosition="top">
+            {renderContent}
+          </Tabs>
+        </View>
+      </View>
+    );
+  }
 }
-
-
-export default First;
+export const title = 'Tabs';
+export const description = 'Tabs example';
