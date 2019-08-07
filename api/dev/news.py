91f1d86b6e6173326b4e36bdac42ecdf1fe80460
zhaochanghui <2636190619@qq.com>
2019-08-08 00:42:12 +0800

    'news'

--- api/dev/news.py
+++ api/dev/news.py
@@ -3,12 +3,101 @@
 
 import requests
 from bs4 import BeautifulSoup
-
+import time
+import pymysql
 
 def write_file(content):
 	f=open("./news.html",'w',encoding='utf8')
 	f.write(content)
 	f.close()
+
+
+
+
+dbstr = ''
+try:
+	f = open('./db.txt', 'r')
+	dbstr = f.read()
+finally:
+	if f:
+		f.close()
+
+
+
+dbdict = eval(dbstr)
+print(dbdict)
+
+host = dbdict['host']
+user = dbdict['user']
+pwd = dbdict['pwd']
+dbname = dbdict['dbname']
+table = dbdict['table']
+port= dbdict['port']
+
+
+
+def add(dic):
+	# 打开数据库连接
+	db =  pymysql.connect(host=host,port=port,user=user,passwd=pwd,db=dbname,use_unicode=True,charset='utf8')
+
+	# 使用 cursor() 方法创建一个游标对象 cursor
+	cursor = db.cursor()
+
+
+	values = "'"+str(dic['addtime'])+"','"+str(dic['title'])+"','"+str(dic['content'])+"','"+str(dic['seemore'])+"','"+str(dic['bearish'])+"','"+str(dic['createtime'])+"','"+str(dic['updatetime'])+"'"+"";
+	sql = "insert into "+table+"(addtime,title,content,seemore,bearish,createtime,updatetime) values ("+values+")"
+	print(sql)
+	result =cursor.execute(sql)
+	db.commit()
+	print(result)
+	# 关闭数据库连接
+	db.close()
+
+
+def findByName(name):
+	db =  pymysql.connect(host=host,port=port,user=user,passwd=pwd,db=dbname,use_unicode=True,charset='utf8')
+
+	cursor = db.cursor()
+
+	sql = "select count(*) as total from "+table+" where name='"+str(name)+"'"
+	print(sql)
+	cursor.execute(sql)
+	data = cursor.fetchall()
+	db.commit()
+
+	has = 1
+	if data[0]==(1,):
+		has = 1
+		print(name+"已经存在")
+	else:
+		has = 0
+
+	db.close()
+	return has
+
+
+def update(sql):
+	# 打开数据库连接
+	db =  pymysql.connect(host=host,port=port,user=user,passwd=pwd,db=dbname,use_unicode=True,charset='utf8')
+
+	# 使用cursor()方法获取操作游标
+	cursor = db.cursor()
+
+	# SQL 更新语句
+	# sql = "UPDATE EMPLOYEE SET AGE = AGE + 1 WHERE SEX = '%c'" % ('M')
+	sql = sql
+	try:
+		# 执行SQL语句
+		cursor.execute(sql)
+		# 提交到数据库执行
+		db.commit()
+		print(sql+'：成功')
+	except:
+		# 发生错误时回滚
+		db.rollback()
+		print(sql+'：失败')
+	# 关闭数据库连接
+	db.close()
 
 
 
@@ -28,16 +117,52 @@ day = soup.select(".time")
 
 allul = soup.select(".live > ul")
 
-for item in allul:
-	print(item)
-	print(item.get("id"))   # id  时间戳
-	print("------------------------------")
-	itemtime = item.select("span")  #时间
-	itemtitle = item.select("h2")  #标题
-	itemcontent = item.select(".lh32 > div") #内容
-	print(itemcontent)
-	break
 
+column ={0:"addtime",1:"title",2:"content",3:'seemore',4:'bearish'}
+
+adddata = {}
+for item in allul:
+	# print(item)
+	adddata['addtime'] = item.get("id")   # id  时间戳
+
+	itemtime = item.select("span")  #时间
+	itemtitle = item.select("h2 > a")  #标题
+	adddata['title'] = itemtitle[0].string
+	itemcontent = item.select(".lh32 > div") #内容
+	content = itemcontent[0].text
+	content = content.strip()
+	adddata['content'] = content
+
+	seemore = item.select(".vote > .seemore > b")[0].text
+	adddata['seemore'] = seemore
+	bearish = item.select(".vote > .bearish > b")[0].text
+	adddata['bearish'] = bearish
+
+
+	now = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+	adddata['createtime'] = now
+	adddata['updatetime'] = now
+
+	add(adddata)
+
+
+
+
+'''
+CREATE TABLE `news` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `title` varchar(256) NOT NULL DEFAULT '' COMMENT '标题',
+  `author` varchar(256) NOT NULL DEFAULT '' COMMENT '作者',
+  `content` text COMMENT '内容',
+  `addtime` varchar(128) DEFAULT '' COMMENT '发布时间',
+  `seemore` varchar(16) DEFAULT '1' COMMENT '看多',
+  `bearish` varchar(16) DEFAULT '1' COMMENT '看空',
+  `source` varchar(128) DEFAULT '1' COMMENT '看空',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='新闻表';
+
+
+'''
 
 
 #时间
--- views/pages/market/list.js
+++ views/pages/market/list.js
@@ -90,7 +90,7 @@ export default class AnatomyExample extends Component {
 
 
     fetchData(){
-        fetch('http://www.developer1.cn:8001/bsj/index.php')
+        fetch('http://www.developer1.cn:8001/bsj/index.php?index=1')
             .then((response) => response.json())
             .then((responseData) => {
                 // 注意，这里使用了this关键字，为了保证this在调用时仍然指向当前组件，我们需要对其进行“绑定”操作
